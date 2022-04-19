b0a05474a9dabe1f2c01530408e0fe1b42d9d8f7
Volodya <volodyahovhannisyan000@gmail.com>
2022-04-19 15:29:47 +0400

    Setting PORT

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
     credentials: true
   })
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
